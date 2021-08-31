77fed4e534c7a76f66588fe6d9364e7cf96f201f
D4R10C5 <vce01866@innovaccion.mx>
2021-08-31 02:37:13 -0500

    se agrega

---
+++ HBD.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+#include<stdio.h>
+#include <conio.h>
+#include <dos.h>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	system("color D");
+ 	setlocale(LC_CTYPE,"Spanish");
+	char h = '\4';
+	cout << endl << endl << endl << endl << endl << endl << "\t\t\t"  << " Espera..." ;
+	Sleep(3002);
+	Beep(523,500);
+	system("cls");
+	//cout << endl << endl << endl << "\t\t\tFrom: \t PROGRAMMING RKR" << endl << "\t\t\tTo:\t Name" << endl << "\t\t";
+	Sleep(500);
+
+for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+	Sleep(500);
+	Beep(523, 500);
+
+	cout << endl << endl << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t  i i i i i" << endl;
+	Sleep(500);
+	Beep(523, 500);
+	cout << "\t\t\t\t__i_i_i_i_i__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t       |" << h << "\t    " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t     __| " << h << "23/04/2021" << h << "|__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t    | " << h << "    " << h << "     " << h << "    " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t  __|" << h << "   "<< h << " FELIZ " << h << "     " << h << "|__";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "     " << h << "CUMPLEAÑOS " << h<< "    "  << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |" << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "  " << h << "|";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t | " << h << "  " << h << "    KARLA   " << h << "     " << h << " |";
+	Sleep(500);
+	Beep(523, 500);
+	cout << endl << "\t\t\t |";
+	for (int i = 0; i < 25; i++) {
+		cout << "_";
+	}
+	cout << "|" << endl<<endl<<endl;
+	
+for (int i = 0; i < 40; i++) {
+		cout << "_";
+	}
+                                                                                                  
+}
